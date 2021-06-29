b15554890ae9c5ebfd49d4cc80412ba99545f4a7
atul kumar <atulfind@gmail.com>
2021-06-29 21:57:28 +0530

    push service changes

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,11 +1,48 @@
 import React from 'react'
+import './index.scss';
+import Theme from '../../assets/img/theme.png';
+import WebDevelopment from '../../assets/img/wedevelopment.png';
+import Mobile from '../../assets/img/mobile.png';
 
 const Services = () => {
+    const serviceData =[
+        {
+            id: 1,
+            icon: Theme,
+            title : "Web Design",
+            desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod."
+        },
+        {
+            id: 2,
+            icon: WebDevelopment,
+            title : "Web Developement",
+            desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod."
+        },
+        {
+            id: 3,
+            icon: Mobile,
+            title : "Mobile Application",
+            desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod."
+        }
+    ]
+
     return (
-        <div>
-            THIS IS SERVICES PAGE
+        <div className="main-container">
+            <div className="services-container">
+           {serviceData.map(item => <Block item={item} key={item.id} />)}
+        </div>
         </div>
     )
 }
 
 export default Services
+
+const Block = ({item})=>{
+    return (
+        <div className="item-container">
+            <img src={item.icon} alt=" Icon Logo " />
+            <div className="item-title title">{item.title}</div>
+            <div className="item-desc desc">{item.desc}</div>
+        </div>
+    )
+}
