fb769d293909000ee94edefd6ba28d784861fe13
MANOJ KUMAR <70262339+manojknp13@users.noreply.github.com>
2021-07-03 14:18:57 +0530

    link hover done

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,31 +6,31 @@ import Pic from '../../assets/img/26.jpg';
 const Navbar = () => {
     return (
 
-                <nav>
-                    <div className="profile-pic">
-                        <img src={Pic} alt="profile pic" />
+        <nav>
+            <div className="profile-pic">
+                <img src={Pic} alt="profile pic" />
+            </div>
+            <ul className="links">
+                <li><NavLink exact={true} to="/" activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+                <li>
+                    <div className="dropdown">
+                        <NavLink to="/service" activeClassName="active" className="dropbtn">Services</NavLink>
+                        <div className="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
                     </div>
-                    <ul className="links">
-                        <li><NavLink  exact={true} to="/" activeClassName="active">Home</NavLink></li>
-                        <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-                        <li>
-                        <div class="dropdown">
-                        <NavLink to="/service" activeClassName="active">Services</NavLink>
-                                <div class="dropdown-content">
-                                <a href="#">Link 1</a>
-                                <a href="#">Link 2</a>
-                                <a href="#">Link 3</a>
-                                </div>
-                                </div>
-                                </li>
-                        <li><NavLink to="/contact" activeClassName="active">Contact Us</NavLink></li>
-                        <li><NavLink to="/works" activeClassName="active">Works</NavLink></li>
-               
-                    </ul>
-                </nav>
-             
-          
-          )
+                </li>
+                <li><NavLink to="/contact" activeClassName="active">Contact Us</NavLink></li>
+                <li><NavLink to="/works" activeClassName="active">Works</NavLink></li>
+
+            </ul>
+        </nav>
+
+
+    )
 }
 
 export default Navbar;
